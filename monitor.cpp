f95eac260265e88db320b4585db145bc11c94e81
Koushik Choudhury <koushik.choudhury@philips.com>
2024-10-01 12:08:15 +0530

    Modified code and tests

--- monitor.cpp
+++ monitor.cpp
@@ -3,36 +3,56 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
-using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+using std::cout;
+using std::flush;
+using std::this_thread::sleep_for;
+using std::chrono::seconds;
+
+namespace {
+    constexpr int TEMP_LIMIT_HIGH = 102;
+    constexpr int TEMP_LIMIT_LOW = 95;
+    constexpr int PULSE_LIMIT_HIGH = 100;
+    constexpr int PULSE_LIMIT_LOW = 60;
+    constexpr int SPO2_LIMIT_LOW = 90;
+}
+
+void DisplayError(std::string error) {
+    cout << error;
+    for (int i = 0; i < 6; i++) {
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
+    }
+}
+
+bool CheckTemperatureVital(float temperature) {
+    if (temperature > TEMP_LIMIT_HIGH || temperature < TEMP_LIMIT_LOW) {
+        DisplayError("Temperature is critical!\n");
+        return false;
+    }
+    return true;
+}
+bool CheckPulseRateVital(float pulseRate) {
+    if (pulseRate < PULSE_LIMIT_LOW || pulseRate >PULSE_LIMIT_HIGH) {
+        DisplayError("Pulse Rate is out of range!\n");
+        return false;
+    }
+    return true;
+}
+bool CheckSPO2Vital(float spo2) {
+    if (spo2 < SPO2_LIMIT_LOW) {
+        DisplayError("Oxygen Saturation out of range!\n");
+        return false;
+    }
+    return true;
+}
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
+    if (CheckTemperatureVital(temperature) &&
+        CheckPulseRateVital(pulseRate) && 
+        CheckSPO2Vital(spo2)) {
+        return 1;
     }
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  }
-  return 1;
 }
--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,8 +1,14 @@
 #include "./monitor.h"
-
 #include "gtest/gtest.h"
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+  ASSERT_FALSE(vitalsOk(99, 102, 70));      //  not-OK- 1
+  ASSERT_FALSE(vitalsOk(103, 80, 94));      //  not-OK- 2
+  ASSERT_FALSE(vitalsOk(98, 105, 94));      //  not-OK- 3
+  ASSERT_FALSE(vitalsOk(98, 80, 70));       //  not-OK- 4
+  ASSERT_FALSE(vitalsOk(103, 105, 94));     //  not-OK- 5
+  ASSERT_FALSE(vitalsOk(103, 80, 70));      //  not-OK- 6
+  ASSERT_FALSE(vitalsOk(98, 105, 70));      //  not-OK- 7
+  ASSERT_FALSE(vitalsOk(103, 105, 70));     //  not-OK- 8
+  ASSERT_TRUE(vitalsOk(98.1f, 70, 98));
 }
