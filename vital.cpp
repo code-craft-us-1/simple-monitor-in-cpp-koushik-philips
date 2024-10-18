de981405a6fae51195d764e41355201f6ff5db54
Koushik Choudhury <koushik.choudhury@philips.com>
2024-10-19 02:09:28 +0530

    Refactoring - moves basline code to another file

--- vital.cpp
+++ vital.cpp
@@ -10,20 +10,6 @@ using std::flush;
 using std::this_thread::sleep_for;
 using std::chrono::seconds;
 
-VitalBaseline::VitalBaseline(float lowLimit, float highLimit,
-    float toleranceLimit) :
-    low(lowLimit), high(highLimit),
-    tolerance(toleranceLimit) {
-    initLevels();
-}
-void VitalBaseline::initLevels() {
-    // Keep levels in sorted order
-    levels.push_back(low);
-    levels.push_back(low + (tolerance * high) / 100);
-    levels.push_back(high - (tolerance * high) / 100);
-    levels.push_back(high);
-}
-
 Vital::Vital(float vitalValue, const VitalBaseline& baseline) :
     value(vitalValue), vitalbase(baseline) {
 }
--- vital.h
+++ vital.h
@@ -3,22 +3,7 @@
 #include <vector>
 #include <set>
 #include "./constants.h"
-
-using VitalConstants::VITALS_MIN;
-using VitalConstants::VITALS_MAX;
-using VitalConstants::TOLERANCE_PERCENT;
-using Levels = std::vector<float>;
-
-struct VitalBaseline {
-    const float low{ VITALS_MIN };
-    const float high{ VITALS_MAX };
-    const float tolerance{ TOLERANCE_PERCENT };
-    Levels levels;
-
-    VitalBaseline(float lowLimit, float highLimit,
-        float toleranceLimit);
-    void initLevels();
-};
+#include "./vitalBaseline.h"
 
 struct Vital {
     float value;
