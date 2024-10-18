ca23d52c6c7e59e5e9aaf3f50339334b09f2fb42
Koushik Choudhury <koushik.choudhury@philips.com>
2024-10-18 15:35:01 +0530

    Refactoring - lint error resolved

--- messageHelper.cpp
+++ messageHelper.cpp
@@ -1,17 +1,17 @@
 #include <string>
-#include "temperature.h"
-#include "pulserate.h"
-#include "spo2.h"
+#include "./temperature.h"
+#include "./pulserate.h"
+#include "./spo2.h"
 #include "./messageHelper.h"
 
 std::string getTemperatureCategoryMessage(int category) {
     auto temperatureCategory = static_cast<Temperature::Category>(category);
     switch (temperatureCategory) {
-    case Temperature::Category::HYPOTHERMIA:     return "Hypothermia : Temperature is critical!\n";
-    case Temperature::Category::NEAR_HYPO:       return "Warning: Approaching hypothermia\n";
-    case Temperature::Category::NORMAL:          return "Normal Temperature\n";
-    case Temperature::Category::NEAR_HYPER:      return "Warning: Approaching hyperthermia\n";
-    case Temperature::Category::HYPERTHERMIA:    return "Hyperthermia : Temperature is critical!\n";
+    case Temperature::Category::HYPOTHERMIA:  return "Hypothermia : Temperature is critical!\n";
+    case Temperature::Category::NEAR_HYPO:    return "Warning: Approaching hypothermia\n";
+    case Temperature::Category::NORMAL:       return "Normal Temperature\n";
+    case Temperature::Category::NEAR_HYPER:   return "Warning: Approaching hyperthermia\n";
+    case Temperature::Category::HYPERTHERMIA: return "Hyperthermia : Temperature is critical!\n";
     default:
         break;
     }
@@ -21,11 +21,11 @@ std::string getTemperatureCategoryMessage(int category) {
 std::string getPulseRateCategoryMessage(int category) {
     auto pulseRateCategory = static_cast<PulseRate::Category>(category);
     switch (pulseRateCategory) {
-    case PulseRate::Category::BRADYCARDIA:      return "Bradycardia : Pulse Rate is out of range!\n";
-    case PulseRate::Category::NEAR_BRADY:       return "Warning: Approaching bradycardia\n";
-    case PulseRate::Category::NORMAL:           return "Normal pule rate\n";
-    case PulseRate::Category::NEAR_TACHY:       return "Warning: Approaching tachycardia\n";
-    case PulseRate::Category::TACHYCARDIA:      return "Tachycardia : Pulse Rate is out of range!\n";
+    case PulseRate::Category::BRADYCARDIA: return "Bradycardia : Pulse Rate is out of range!\n";
+    case PulseRate::Category::NEAR_BRADY:  return "Warning: Approaching bradycardia\n";
+    case PulseRate::Category::NORMAL:      return "Normal pule rate\n";
+    case PulseRate::Category::NEAR_TACHY:  return "Warning: Approaching tachycardia\n";
+    case PulseRate::Category::TACHYCARDIA: return "Tachycardia : Pulse Rate is out of range!\n";
     default:
         break;
     }
@@ -35,11 +35,11 @@ std::string getPulseRateCategoryMessage(int category) {
 std::string getSPO2CategoryMessage(int category) {
     auto spo2category = static_cast<SPO2::Category>(category);
     switch (spo2category) {
-    case SPO2::Category::HYPOAXEMIA:            return "Hypoaxemia : Oxygen Saturation out of range!\n";
-    case SPO2::Category::NEAR_HYPOAXEMIA:       return "Warning: Approaching hypoaxemia\n";
-    case SPO2::Category::NORMAL:                return "Normal oxygen saturation\n";
-    case SPO2::Category::NEAR_HYPEROAXEMIA:     return "Warning: Approaching hyperoaxemia\n";
-    case SPO2::Category::HYPEROAXEMIA:          return "Hyperoaxemia : Oxygen Saturation out of range!\n";
+    case SPO2::Category::HYPOAXEMIA:        return "Hypoaxemia : Oxygen Saturation out of range!\n";
+    case SPO2::Category::NEAR_HYPOAXEMIA:   return "Warning: Approaching hypoaxemia\n";
+    case SPO2::Category::NORMAL:            return "Normal oxygen saturation\n";
+    case SPO2::Category::NEAR_HYPEROAXEMIA: return "Warning: Approaching hyperoaxemia\n";
+    case SPO2::Category::HYPEROAXEMIA:      return "Hyperoaxemia : Oxygen Saturation out of range!\n";
     default:
         break;
     }
